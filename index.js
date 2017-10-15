723d16c0c145789519bc6f3a2f3d50c8c87773b9
Maks <pvl1720@gmail.com>
2017-10-15 15:42:40 -0700

    progress

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
-require('./lib/greet.js');
+const greet = require('./lib/greet.js');
+const domath = require('./lib/arithmetic.js');
 
-console.log('msg from index.js. following classe lecture example');
+console.log(greet('Max'));
+
+console.log('msg from index.js');
+
+console.log(domath.add(1,7));
+console.log(domath.subtract(1,4));
