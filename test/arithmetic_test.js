b71fb151c8defa8539f189ba8332bc667971d9f0
Maks <pvl1720@gmail.com>
2017-11-14 05:14:07 -0800

    works

--- test/arithmetic_test.js
+++ test/arithmetic_test.js
@@ -6,6 +6,11 @@ const expect = require('expect');
 describe('Arithmetic', function() {
   it('can add two numbers properly', function() {
     let results = arithmetic.add(1,7);
-    expect(results).toEqual(8);
+    expect(results).toEqual(9);
+  });
+
+  it('should be null if one isn\'t a number', function() {
+    let results = arithmetic.add('Aye',7);
+    expect(results).toEqual(null);
   });
 });
